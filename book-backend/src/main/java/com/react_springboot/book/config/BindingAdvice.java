1e07f96d7057e2a40ecef7899f089a578a90ad0f
Shim0209 <81503611+Shim0209@users.noreply.github.com>
2021-06-08 02:45:04 +0900

    Update log파일 저장 기능 추가

--- book-backend/src/main/java/com/react_springboot/book/config/BindingAdvice.java
+++ book-backend/src/main/java/com/react_springboot/book/config/BindingAdvice.java
@@ -4,17 +4,25 @@ import com.react_springboot.book.domain.CommonDto;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
 @Aspect
 @Component
 public class BindingAdvice {
+
+    private static final Logger log = LoggerFactory.getLogger(BindingAdvice.class);
 
     /**
      * 유효성검사 함수
@@ -24,9 +32,14 @@ public class BindingAdvice {
      *      유효성에 문제 발생 : CommonDto(에러코드 400, 에러메세지)
      *      문제 없음 : 요청에 맞는 컨트롤러 함수 호출
      * @throws Throwable
+     *
+     * 정상적인 접근에서는 동작하지 않음. (프론트에서 유효성검사를 하기때문에)
+     * 비정상적인 접근시 막기 위함. Postman사용, http요청 오류 등
      */
     @Around("execution(* com.react_springboot.book.web..*Controller.*(..))")
     public Object validCheck(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        String type = proceedingJoinPoint.getSignature().getDeclaringTypeName();
+        String method = proceedingJoinPoint.getSignature().getName();
         // 호출된 함수의 매개변수 정보 추출
         Object[] args = proceedingJoinPoint.getArgs();
 
@@ -43,6 +56,7 @@ public class BindingAdvice {
                     // 에러메시지가 발생한 필드와 해당 에러메시지를 errorMap에 담기
                     for (FieldError error : bindingResult.getFieldErrors()) {
                         errorMap.put(error.getField(), error.getDefaultMessage());
+                        log.warn(type+"."+method+"() => 필드 : "+error.getField()+", 메시지 : "+error.getDefaultMessage());
                     }
 
                     // 에러메시지 반환
@@ -54,4 +68,15 @@ public class BindingAdvice {
         // 유효성검사에 문제가 없을시 다음 호출 정상진행
         return proceedingJoinPoint.proceed();
     }
+
+//    /**
+//     *  AOP 테스트용
+//     */
+//    @Before("execution(* com.react_springboot.book.web..*Controller.*(..))")
+//    public void testCheck() {
+//        HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+//        System.out.println("주소 : " + request.getRequestURI());
+//        System.out.println("메서드 : " + request.getMethod());
+//        System.out.println("전처리 로그를 남겼습니다.");
+//    }
 }
